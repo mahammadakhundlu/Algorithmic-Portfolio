21bbdeacd426dd502407741fd820a2f9a3210c4e
akhundlu <mahammad.akhundlu@gmail.com>
2025-10-24 20:21:15 +0100

    Add solution for dice combinations problem
    Implement dynamic programming to calculate dice combinations.

--- Dice-Combinations/solution.cpp
+++ Dice-Combinations/solution.cpp
@@ -1,1 +1,16 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+long a[1000001];
+int main()
+{
+   long n;
+   cin>>n;
+   a[0]=1;
+   for(long i=1;i<=n;i++){
+    for(long j=1;j<=6;j++){
+    if(i-j>-1)
+    a[i]=(a[i]+a[i-j])%1000000007;
+    }
+   }
+   cout<<a[n];
+    return 0;
